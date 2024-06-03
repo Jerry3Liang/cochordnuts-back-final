01ba9cf42302541a2aac44b9a36b1fe75f5638f2
lin888688 <164441308+lin888688@users.noreply.github.com>
2024-06-03 20:15:58 +0800

    Update member-routes.js

--- src/views/Member/member-routes.js
+++ src/views/Member/member-routes.js
@@ -1,7 +1,9 @@
 //引入 SFC 元件
 import Members from './Members.vue';
+import Member from './Member.vue';
 
 //設定路由網址
 export default [
-    { name: "members-link", path: "/members", component: Members }
+    { name: "members-link", path: "/members", component: Members },
+    { name: "member-link", path: "/member", component: Member }
 ];
